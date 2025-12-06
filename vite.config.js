6da2a2b89a4e71509e245435d0a938b8eb35af72
rahul <murahul76@gmail.com>
2025-12-06 18:02:40 +0530

    ghfj

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/banking/',
+ 
   plugins: [react()],
+   base: '/bankingweb/',
 })
